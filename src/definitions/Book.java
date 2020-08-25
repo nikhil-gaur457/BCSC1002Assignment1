5a64e4064fb1d385625cde783f0c3910ae07b4c5
Nikhil Gaur <nikhil.gaur_cs19@gla.ac.in>
2020-08-25 22:28:21 +0530

    create private fields for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -7,4 +7,31 @@
 package definitions;
 
 public class Book {
+    private String nameOfTheBook;
+    private String nameOfTheAuthorOfTheBook;
+    private String thirteenDigitISBNNumberOfTheBook;
+
+    public String getNameOfTheBook() {
+        return nameOfTheBook;
+    }
+
+    public void setNameOfTheBook(String nameOfTheBook) {
+        this.nameOfTheBook = nameOfTheBook;
+    }
+
+    public String getNameOfTheAuthorOfTheBook() {
+        return nameOfTheAuthorOfTheBook;
+    }
+
+    public void setNameOfTheAuthorOfTheBook(String nameOfTheAuthorOfTheBook) {
+        this.nameOfTheAuthorOfTheBook = nameOfTheAuthorOfTheBook;
+    }
+
+    public String getThirteenDigitISBNNumberOfTheBook() {
+        return thirteenDigitISBNNumberOfTheBook;
+    }
+
+    public void setThirteenDigitISBNNumberOfTheBook(String thirteenDigitISBNNumberOfTheBook) {
+        this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
+    }
 }
