f29f6d25dbc6ca3bd8909c2dacebe356adaa8be7
Nikhil Gaur <nikhil.gaur_cs19@gla.ac.in>
2020-08-25 22:30:37 +0530

    create getter and setter method for private fields in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,27 +11,5 @@ public class Book {
     private String nameOfTheAuthorOfTheBook;
     private String thirteenDigitISBNNumberOfTheBook;
 
-    public String getNameOfTheBook() {
-        return nameOfTheBook;
-    }
 
-    public void setNameOfTheBook(String nameOfTheBook) {
-        this.nameOfTheBook = nameOfTheBook;
-    }
-
-    public String getNameOfTheAuthorOfTheBook() {
-        return nameOfTheAuthorOfTheBook;
-    }
-
-    public void setNameOfTheAuthorOfTheBook(String nameOfTheAuthorOfTheBook) {
-        this.nameOfTheAuthorOfTheBook = nameOfTheAuthorOfTheBook;
-    }
-
-    public String getThirteenDigitISBNNumberOfTheBook() {
-        return thirteenDigitISBNNumberOfTheBook;
-    }
-
-    public void setThirteenDigitISBNNumberOfTheBook(String thirteenDigitISBNNumberOfTheBook) {
-        this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
-    }
 }
